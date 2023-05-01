7753a3839772bcab3f6e813dfb89238dc326b636
arcane <arcanesir.neo@gmail.com>
2023-05-01 09:01:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { uhu } from 'uhugrid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
